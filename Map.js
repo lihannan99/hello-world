d4c8992391790fef9f38e4fc3e42217a9fe490c1
baochen hu <baochenhu@gmail.com>
2014-10-03 13:07:58 -0400

    This is javascript file for test

---
+++ Map.js
@@ -0,0 +1,11 @@
+
+    function doubleAll(numbers) {
+      // SOLUTION GOES 
+       var result=numbers.map(function double(num){
+       	    return num*2;
+       })
+       return result;
+
+    }
+    
+    module.exports = doubleAll
